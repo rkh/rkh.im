4b9de251b03c4af556142abaf73d2236b68134b8
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-08-27 12:40:34 +0200

    fix robots.txt

--- pages.rb
+++ pages.rb
@@ -15,4 +15,4 @@ end
 
 Blog::Page.new("templates/blog.sass", "/blog.css", 200, false, Compass.sass_engine_options).header["Content-Type"] = "text/css"
 Blog::Page.new("templates/feed.builder", "/feed.xml", 200, false).header["Content-Type"] = "text/xml"
-Blog::Page.new("User-agent: *\nDisallow: /\n", "/robots.txt", 200, false).header["Content-Type"] = "text/plain"
+Blog::Page.new("User-agent: *\nAllow: /\n", "/robots.txt", 200, false).header["Content-Type"] = "text/plain"
